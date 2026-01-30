bc926cfc9d6df346d16095a8ecb1884ccbd245f2
YagamiLight <2596484860@qq.com>
2026-01-30 15:00:19 +0800

    初始化项目基础结构

---
+++ src/main/java/com/zyyyys/culinarywhispers/CulinaryWhispersApplication.java
@@ -0,0 +1,13 @@
+package com.zyyyys.culinarywhispers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CulinaryWhispersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CulinaryWhispersApplication.class, args);
+    }
+
+}
